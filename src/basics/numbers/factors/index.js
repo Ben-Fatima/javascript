727f75137795bf5d6a0c72ab2a655eb2dbc5d533
Oressama <fatimazahra@gmail.com>
2020-07-18 19:48:16 +0100

    Solve numbers/factors

--- src/basics/numbers/factors/index.js
+++ src/basics/numbers/factors/index.js
@@ -3,7 +3,19 @@
  * @returns {Array<Number>} the list of factors of `n`.
  */
 function getFactors(n) {
-  // write your code here
-}
+  factors = [];
+  if( n == 0 || n ==1){
+    return [n];
+  }
+  for (let i = 2; i <= n; i++) {
+    while (n%i == 0) {
+      factors.push(i);
+      n = n/i ;
+    }
+    
+  }
+  return factors ;
+  }
+    
 
 module.exports = getFactors;
