b0341675b5fa619494e883c27d260339e6a62824
Oressama <fatimazahra@gmail.com>
2020-07-18 20:48:28 +0100

    solve arrays/intersection

--- src/basics/arrays/intersection/index.js
+++ src/basics/arrays/intersection/index.js
@@ -4,6 +4,17 @@
  * @returns {Array<Number>} The intersection of the two lists
  * Bonus: try to implement this without a `for` or `while` loop
  */
-function intersection(listA, listB) {}
+function intersection(listA, listB) {
+    arrayNumb = [];
+    for (let i = 0; i < listA.length; i++) {
+        for (let j = 0; j < listB.length; j++) {
+            if (listA[i]==listB[j]) {
+               arrayNumb.push(listA[i]) 
+            }   
+        }
+        
+    }
+    return arrayNumb ;
+}
 
 module.exports = intersection;
