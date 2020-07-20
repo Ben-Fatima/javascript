c739697c3cda5610f17b01a64b361f1b2323a1be
Oressama <fatimazahra@gmail.com>
2020-07-20 04:34:46 +0100

    Solve arrays/only-positive

--- src/basics/arrays/only-positive/index.js
+++ src/basics/arrays/only-positive/index.js
@@ -4,6 +4,13 @@
  * onlyPositivve([1, -5, 3, 5, -1, 0, 4]) //=> [1, 3, 5, 0, 4]
  * Bonus: try to implement this without a `for` or `while` loop
  */
-function onlyPositivve(numbers) {}
-
+function onlyPositivve(numbers) {
+    var result = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] >= 0) {
+            result.push(numbers[i] )
+        }
+    }
+    return result
+}
 module.exports = onlyPositivve;
