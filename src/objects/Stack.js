59c30b83e921e6ec4fde5920c9f378088525bb09
Ben-Fatima <fatimazahra@gmail.com>
2020-07-22 22:28:43 +0100

    solve stack obj

--- src/objects/Stack.js
+++ src/objects/Stack.js
@@ -2,7 +2,19 @@
  * Implements the stack data structure.
  */
 class Stack {
-  push(element) {}
-  peek() {}
-  pop() {}
+  constructor() {
+    this.data = [] ;
+    this.lastIndex = 0 ;
+  }
+  push(element) {
+    this.data[this.lastIndex] = element ;
+    this.lastIndex += 1 ;
+  }
+  peek() {
+    return this.data[this.lastIndex - 1] ;
+  }
+  pop() {
+    this.lastIndex -= 1 ;
+    return this.data.pop();  
+  }
 }
